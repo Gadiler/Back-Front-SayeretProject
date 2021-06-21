5c315c0a938796e6acfd692d4838ef88f65569b6
Gadi <62388856+Gadiler@users.noreply.github.com>
2021-06-21 22:43:29 +0300

    First commit

---
+++ src/main/java/com/example/sayeretproject/beans/Area.java
@@ -0,0 +1,24 @@
+package com.example.sayeretproject.beans;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "areas")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Area {
+    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @Singular
+    private List<Point> points = new ArrayList<>();
+}
